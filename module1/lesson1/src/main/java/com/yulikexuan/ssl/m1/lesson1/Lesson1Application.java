f15a455ac2151030b17cbe5bb69ecf9c5d169c4c
yul <yu.li@tecsys.com>
2019-06-01 23:06:20 -0400

    Added application class for lesson 1 of module 1

---
+++ module1/lesson1/src/main/java/com/yulikexuan/ssl/m1/lesson1/Lesson1Application.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.ssl.m1.lesson1.Lesson1Application.java
+
+
+package com.yulikexuan.ssl.m1.lesson1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Lesson1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson1Application.class, args);
+    }
+
+}///:~

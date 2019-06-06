a07c51ee3bbcc170d03d3cda5a2c01fa9bc6cbeb
Yu Li <yu.li@tecsys.com>
2019-06-06 15:43:11 -0400

    Add logs to see anonymous authentication of login page

--- ssl-module1/lesson6/src/main/java/com/yulikexuan/ssl/m1/lesson6/app/v1/controllers/PathController.java
+++ ssl-module1/lesson6/src/main/java/com/yulikexuan/ssl/m1/lesson6/app/v1/controllers/PathController.java
@@ -4,15 +4,26 @@
 package com.yulikexuan.ssl.m1.lesson6.app.v1.controllers;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+@Slf4j
 @Controller
 public class PathController {
 
     @RequestMapping("/login")
     public String list() {
+
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        log.info(">>>>>>> Principal: {}", authentication.getPrincipal());
+        log.info(">>>>>>> Authorities: {}", authentication.getAuthorities());
+
         return "loginPage";
     }
 

2f5e2c83eb8a4f1c53a935dc6bb3b4382d781158
yul <yu.li@tecsys.com>
2019-06-09 21:19:55 -0400

    Complete OAuth2-JWT Setup

--- ssl-module12/lesson2/src/main/java/com/yulikexuan/ssl/app/config/security/AuthorizationServerConfiguration.java
+++ ssl-module12/lesson2/src/main/java/com/yulikexuan/ssl/app/config/security/AuthorizationServerConfiguration.java
@@ -6,17 +6,18 @@ package com.yulikexuan.ssl.app.config.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 
 @Slf4j
@@ -24,6 +25,9 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration
         extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${signing-key:oui214hmui23o4hm1pui3o2hp4m1o3h2m1o43}")
+    private String signingKey;
 
     private final AuthenticationManager authenticationManager;
 
@@ -34,6 +38,22 @@ public class AuthorizationServerConfiguration
         super();
 
         this.authenticationManager = authenticationManager;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+
+        final JwtAccessTokenConverter jwtAccessTokenConverter =
+                new JwtAccessTokenConverter();
+
+        jwtAccessTokenConverter.setSigningKey(signingKey);
+
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Override
@@ -52,14 +72,10 @@ public class AuthorizationServerConfiguration
     public void configure(AuthorizationServerEndpointsConfigurer endpoints)
             throws Exception {
 
-        endpoints.tokenStore(tokenStore()).
-                authenticationManager(this.authenticationManager).
-                allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        endpoints.tokenStore(tokenStore())
+                .authenticationManager(this.authenticationManager)
+                .accessTokenConverter(this.accessTokenConverter())
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
     }
 
 }///:~

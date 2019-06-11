34a937632b9768a7ae92a639496cd683a13f8c1a
Yu Li <yu.li@tecsys.com>
2019-06-11 09:36:50 -0400

    Add README.md to lesson1 of module12

--- ssl-module12/lesson1/src/main/java/com/yulikexuan/ssl/app/config/security/LssSecurityConfig.java
+++ ssl-module12/lesson1/src/main/java/com/yulikexuan/ssl/app/config/security/LssSecurityConfig.java
@@ -60,8 +60,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().
-                csrf()
+                .and()
+                .csrf()
                 .disable();
     }
 

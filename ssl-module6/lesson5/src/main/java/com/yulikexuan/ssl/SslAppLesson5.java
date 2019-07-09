01eb6459d4229b3720604984ae77349053ce0c48
Yu Li <yu.li@tecsys.com>
2019-07-09 07:36:11 -0400

    Prepare Lesson5: custom pw encoder

---
+++ ssl-module6/lesson5/src/main/java/com/yulikexuan/ssl/SslAppLesson5.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.ssl.SslAppLesson5.java
+
+
+package com.yulikexuan.ssl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SslAppLesson5 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslAppLesson5.class, args);
+    }
+
+}///:~
---
+++ ssl-module6/lesson5/src/main/java/com/yulikexuan/ssl/domain/services/EmailExistsException.java
@@ -0,0 +1,17 @@
+//: com.yulikexuan.ssl.domain.services.EmailExistsException.java
+
+
+package com.yulikexuan.ssl.domain.services;
+
+
+public class EmailExistsException extends RuntimeException {
+
+    public EmailExistsException() {
+        super();
+    }
+
+    public EmailExistsException(String message) {
+        super(message);
+    }
+
+}///:~

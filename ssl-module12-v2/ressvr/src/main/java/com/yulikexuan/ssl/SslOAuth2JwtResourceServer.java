ac72395d2c28691cf81d1f3a12df171fb9786b10
Yu Li <yu.li@tecsys.com>
2019-09-11 09:32:08 -0400

    Add resource server

---
+++ ssl-module12-v2/ressvr/src/main/java/com/yulikexuan/ssl/SslOAuth2JwtResourceServer.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.ssl.SslApp.java
+
+
+package com.yulikexuan.ssl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SslOAuth2JwtResourceServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslOAuth2JwtResourceServer.class, args);
+    }
+
+}///:~
